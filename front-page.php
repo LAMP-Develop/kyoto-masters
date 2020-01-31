a11fb09156610bcdad88f4b5a0e34d10f9250413
ShunyaSuzuki <anyushu2017@gmail.com>
2020-01-31 11:23:20 +0900

    update

--- front-page.php
+++ front-page.php
@@ -10,15 +10,20 @@ get_header(); ?>
 $lang_flag = false;
 if ($lang === 'en') {
     $ttl = 'Media that delivers information on Kyoto';
+    $migoro_str = 'Best time';
 } elseif ($lang === 'ko') {
     $ttl = '교토의 정보 전달 매체';
+    $migoro_str = '예년의 절정';
 } elseif ($lang === 'zh-hans') {
     $ttl = '传播京都信息的媒体';
+    $migoro_str = '一年中最好的时间';
 } elseif ($lang === 'zh-hant') {
     $ttl = '傳播京都信息的媒體';
+    $migoro_str = '一年中最好的時間';
 } else {
     $lang_flag = true;
     $ttl = '京都の情報を届けるメディア';
+    $migoro_str = '例年の見頃';
 }
 ?>
 <h2 class="mincho w-normal"><?php echo $ttl; ?></h2>
@@ -30,6 +35,72 @@ if ($lang === 'en') {
 <a href="#" class="hot-word">タグ名</a>
 <a href="#" class="hot-word">タグ名</a>
 </div>
+</div>
+</div>
+</section>
+
+<section id="flowering" class="sec">
+<div class="wrap">
+<?php if ($lang_flag): ?>
+<h2 class="ttl2 txt-c"><span class="color-sky d-block">開花情報</span><span>FLOWERING INFO</span></h2>
+<?php else: ?>
+<h2 class="ttl2 txt-c"><span class="color-sky d-block">FLOWERING INFO</span></h2>
+<?php endif; ?>
+<ul class="post-list flowering-list mb-2">
+<?php
+$args = [
+  'posts_per_page' => 12,
+  'orderby' => 'date',
+  'order' => 'DESC',
+  'post_type' => 'flowering_info',
+];
+query_posts($args);
+while (have_posts()): the_post();
+$p = get_the_permalink();
+$t = get_the_title();
+if (has_post_thumbnail()) {
+    $i = get_the_post_thumbnail_url(get_the_ID(), 'large');
+    $i_l = get_the_post_thumbnail_url(get_the_ID(), 'full');
+} else {
+    $i = $wp_url.'/lib/images/no-img.png';
+    $i_l = $wp_url.'/lib/images/no-img.png';
+}
+if (get_field('migoro', get_the_ID())) {
+    $migoro = get_field('migoro', get_the_ID());
+} else {
+    $migoro = '';
+}
+if (get_field('flower_level', get_the_ID())) {
+    $flower_level = get_field('flower_level', get_the_ID());
+} else {
+    $flower_level = '';
+}
+?>
+<li>
+<a class="relative" href="<?php echo $p; ?>">
+<img src="<?php echo $i; ?>" srcset="<?php echo $i; ?> 1x,<?php echo $i_l; ?> 2x" alt="<?php echo $t; ?>">
+<div class="flower-info">
+<div class="flower">
+<span><img src="<?php echo $wp_url; ?>/lib/images/sakura_<?php echo $flower_level['value']; ?>.svg" alt="桜の開花情報"></span>
+<span><?php echo $flower_level['label']; ?></span>
+</div>
+<div class="migoro">
+<span><?php echo $migoro_str; ?><br><?php echo $migoro; ?></span>
+</div>
+</div>
+<div class="txt">
+<h3><?php echo $t; ?></h3>
+</div>
+</a>
+</li>
+<?php endwhile; wp_reset_query(); ?>
+</ul>
+<div class="txt-c">
+<?php if ($lang_flag): ?>
+<a href="<?php echo $home; ?>/new-post/" class="btn">すべての開花情報を見る</a>
+<?php else: ?>
+<a href="<?php echo $home; ?>/new-post/" class="btn">More</a>
+<?php endif; ?>
 </div>
 </div>
 </section>
@@ -80,7 +151,7 @@ $category = get_the_category();
 </ul>
 <div class="txt-c">
 <?php if ($lang_flag): ?>
-<a href="<?php echo $home; ?>/new-post/" class="btn">一覧を見る</a>
+<a href="<?php echo $home; ?>/new-post/" class="btn">すべての記事記事を見る</a>
 <?php else: ?>
 <a href="<?php echo $home; ?>/new-post/" class="btn">More</a>
 <?php endif; ?>
---
+++ single-flowering_info.php
@@ -0,0 +1,171 @@
+<?php
+$home = esc_url(home_url());
+$wp_url = get_template_directory_uri();
+get_header();
+
+$lang = ICL_LANGUAGE_CODE;
+if ($lang === 'en') {
+    $modified_time = 'Updated date';
+} elseif ($lang === 'ko') {
+    $modified_time = '업데이트 날짜';
+} elseif ($lang === 'zh-hans') {
+    $modified_time = '更新日期';
+} elseif ($lang === 'zh-hant') {
+    $modified_time = '更新日期';
+} else {
+    $modified_time = '更新日';
+}
+
+if (have_posts()): the_post();
+// 記事情報
+$t = get_the_title();
+$p = get_the_permalink();
+$id = get_the_ID();
+$time = get_the_modified_time('Y-m-d');
+if (has_post_thumbnail()) {
+    $i = get_the_post_thumbnail_url(get_the_ID(), 'large');
+    $i_l = get_the_post_thumbnail_url(get_the_ID(), 'full');
+} else {
+    $i = $wp_url.'/lib/images/no-img.png';
+    $i_l = $wp_url.'/lib/images/no-img.png';
+}
+?>
+<section class="sec">
+<div id="single-wrap" class="wrap main-wrap">
+<article id="main-content" class="post-main">
+<div class="i-catch">
+<img src="<?php echo $i; ?>" srcset="<?php echo $i; ?> 1x,<?php echo $i_l; ?> 2x" alt="<?php echo $t; ?>">
+</div>
+<h2 class="postttl"><?php echo $t; ?></h2>
+<div class="meta">
+<time class="color-sky" datetime="<?php echo $time; ?>"><?php echo $modified_time; ?>：<?php the_modified_time('Y.m.d'); ?></time>
+</div>
+<!-- メインコンテンツ -->
+<div class="post-inner mb-3">
+<?php the_content(); ?>
+<!-- 施設概要 -->
+<table>
+<tbody>
+<?php if(get_field('build-name')): ?>
+<tr><th>施設名</th>
+<td><?php the_field('build-name'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('address')): ?>
+<tr><th>住所</th>
+<td><?php the_field('address'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('tell')): ?>
+<tr><th>電話番号</th>
+<td><?php the_field('tell'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('station')): ?>
+<tr><th>最寄り駅</th>
+<td><?php the_field('station'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('url')): ?>
+<tr><th>URL</th>
+<td><?php the_field('url'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('bas')): ?>
+<tr><th>最寄りバス停</th>
+<td><?php the_field('bas'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('time')): ?>
+<tr><th>営業時間</th>
+<td><?php the_field('time'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('holiday')): ?>
+<tr><th>定休日</th>
+<td><?php the_field('holiday'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('money')): ?>
+<tr><th>入場料</th>
+<td><?php the_field('money'); ?></td></tr>
+<?php endif; ?>
+<?php if(get_field('other')): ?>
+<tr><th>備考</th>
+<td><?php the_field('other'); ?></td></tr>
+<?php endif; ?>
+</tbody>
+</table>
+
+<?php if (get_field('geocode')): ?>
+<div class="gmap">
+<iframe src="https://www.google.com/maps?q=<?php echo get_field('geocode'); ?>&hl=jp&output=embed"></iframe>
+</div>
+<?php endif; ?>
+
+</div>
+
+<?php if ($tags != ''): ?>
+<div class="single-tags">
+<h3>この記事のタグ</h3>
+<ul>
+<?php foreach ($tags as $key => $tag): ?>
+<li>
+<a href="<?php echo $home.'/tag/'.$tag->slug; ?>"><i class="fas fa-tag"></i> <?php echo $tag->name; ?></a>
+</li>
+<?php endforeach; ?>
+</ul>
+</div>
+<?php endif; ?>
+
+<div class="sns-share mt-3 mb-3">
+<p class="mb-1 txt-c sns-share-str">この記事が気に入ったら<span>SNSでシェアしよう！</span></p>
+<ul class="txt-c">
+<li class="fb">
+<a href="https://www.facebook.com/sharer.php?src=bm&u=<?php echo $p; ?>&t=<?php echo $t; ?>" target="_blank">
+<i class="fab fa-facebook-f"></i>
+</a>
+</li>
+<li class="tw">
+<a href="https://twitter.com/intent/tweet?url=<?php echo $p; ?>&text=<?php echo $t; ?>" target="_blank">
+<i class="fab fa-twitter"></i>
+</a>
+</li>
+<li class="pi">
+<a rel="nofollow" target="_blank" href="http://www.pinterest.com/pin/create/button/?url=<?php echo $p; ?>&media=<?php echo $i_l; ?>&description=">
+<i class="fab fa-pinterest-p"></i>
+</a>
+</li>
+</ul>
+</div>
+
+<section id="relation-post">
+<h3 class="ttl3">関連記事</h3>
+<ul class="post-list">
+<?php
+$args = [
+  'post_type' => 'flowering_info',
+  'posts_per_page' => '4',
+  'orderby' => 'rand'
+];
+$posts = get_posts($args);
+foreach ($posts as $post): setup_postdata($post);
+$p = get_the_permalink();
+$t = get_the_title();
+if (has_post_thumbnail()) {
+    $i = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+    $i_l = get_the_post_thumbnail_url(get_the_ID(), 'large');
+} else {
+    $i = $wp_url.'/lib/images/no-img.png';
+    $i_l = $wp_url.'/lib/images/no-img.png';
+}
+?>
+<li>
+<a href="<?php echo $p; ?>">
+<img src="<?php echo $i; ?>" srcset="<?php echo $i; ?> 1x,<?php echo $i_l; ?> 2x" alt="<?php echo $t; ?>">
+<div class="txt">
+<h3><?php echo $t; ?></h3>
+</div>
+</a>
+</li>
+<?php endforeach; wp_reset_postdata(); ?>
+</ul>
+</section>
+</article>
+<!-- サイドバー -->
+<?php get_sidebar(); ?>
+</div>
+</section>
+<?php endif; get_footer();
