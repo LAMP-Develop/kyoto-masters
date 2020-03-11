be9a56fadf8085ce3f65bd2dfa97df041af57892
ShunyaSuzuki <anyushu2017@gmail.com>
2020-03-11 19:11:43 +0900

    Add app links

--- templates/cta-foot.php
+++ templates/cta-foot.php
@@ -2,23 +2,23 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri(); ?>
 <section class="foot-cta">
-  <div class="wrap">
-    <div class="cta-wrap">
-      <div class="img">
-        <img src="<?php echo $wp_url; ?>/lib/images/cta_sp_pic.png" alt="「MKスマホ配車」でいつでも配車">
-      </div>
-      <div class="txt">
-        <h2>「MKスマホ配車」<br class="sp-only">でいつでも配車</h2>
-        <p>スマホアプリからだから電話の必要なし！</p>
-        <div class="btns">
-          <a href="#" target="_blank">
-            <img src="<?php echo $wp_url; ?>/lib/images/google_play.png" alt="Google Play">
-          </a>
-          <a href="#" target="_blank">
-            <img src="<?php echo $wp_url; ?>/lib/images/app_store.png" alt="App Store">
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="wrap">
+<div class="cta-wrap">
+<div class="img">
+<img src="<?php echo $wp_url; ?>/lib/images/cta_sp_pic.png" alt="「MKスマホ配車」でいつでも配車">
+</div>
+<div class="txt">
+<h2>「MKスマホ配車」<br class="sp-only">でいつでも配車</h2>
+<p>スマホアプリからだから電話の必要なし！</p>
+<div class="btns">
+<a href="https://play.google.com/store/apps/details?id=jp.co.mk_group.smart.Main" target="_blank">
+<img src="<?php echo $wp_url; ?>/lib/images/google_play.png" alt="Google Play">
+</a>
+<a href="https://itunes.apple.com/jp/app/mksumaho-pei-che/id685813334?mt=8" target="_blank">
+<img src="<?php echo $wp_url; ?>/lib/images/app_store.png" alt="App Store">
+</a>
+</div>
+</div>
+</div>
+</div>
 </section>
